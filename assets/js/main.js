d4f64a11cf166d0f55cce5cf62e29e214a231b8d
Ravasys <ravasys@gmail.com>
2025-02-16 14:46:19 -0300

    ajustes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,24 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById('mode').addEventListener('click', () => {
-        if (document.body.classList.contains('dark')) {
-          document.body.classList.remove('dark');
-          localStorage.removeItem('mode');
-          document.getElementById('mode').classList.remove('bi-sun-fill');
-          document.getElementById('mode').classList.add('bi-moon-fill');
-      
-        }
-        else {
-          document.body.classList.add('dark');
-          localStorage.setItem('mode', 'dark');
-          document.getElementById('mode').classList.remove('bi-moon-fill');
-          document.getElementById('mode').classList.add('bi-sun-fill');  
-        }
-      });    
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById('mode').addEventListener('click', () => {
+    if (document.body.classList.contains('dark')) {
+      document.body.classList.remove('dark');
+      localStorage.removeItem('mode');
+      document.getElementById('mode').classList.remove('bi-sun-fill');
+      document.getElementById('mode').classList.add('bi-moon-fill');
+
+    }
+    else {
+      document.body.classList.add('dark');
+      localStorage.setItem('mode', 'dark');
+      document.getElementById('mode').classList.remove('bi-moon-fill');
+      document.getElementById('mode').classList.add('bi-sun-fill');
+    }
+  });
+
+  if (localStorage.getItem('mode') != undefined) {
+    document.body.classList.add(localStorage.getItem('mode'));
+    document.getElementById('mode').classList.remove('bi-moon-fill');
+    document.getElementById('mode').classList.add('bi-sun-fill');
+  }
+
 });
